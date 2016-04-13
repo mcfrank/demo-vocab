4dedd124034e73eeea2492a151fda6dbcd6c25ae
Mika Braginsky <mikabr@stanford.edu>
2016-04-13 16:02:10 -0700

    bit more render script tweaks

--- render.sh
+++ render.sh
@@ -8,6 +8,8 @@ R CMD BATCH render.R;
 mv out ..
 git checkout gh-pages
 mv ../out/* .
+rm -r ../out
 git add .
 git commit -m "deployed to github pages"
 git push --force --quiet master:gh-pages
+git checkout master
